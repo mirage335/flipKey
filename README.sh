0780c68be97679534724ddaefd2633dc9490e47a
mirage335 <spamfreemirage335@gmail.com>
2021-12-28 23:22:50 -0500

    first commit

---
+++ README.sh
@@ -0,0 +1,322 @@
+#!/usr/bin/env bash
+
+# Dependencies.
+# May need 'ubiquitous_bash.sh" in "$PATH".
+# GNU Octave, Qalculate - usually dependency of 'calculator' scripts
+# recode - usually dependency of 'markup documentation' scripts
+# wkhtmltopdf - may be necessary for accurate conversion from HTML to PDF
+
+# NOTICE: README !
+# 
+# 
+# 
+# NOTICE: README !
+
+# CAUTION: As a user, you should have been provided a virtual machine or cloud services to run this script - 'ubiquitous bash' provides functions to ease the use of either and both. An SELinux, AppArmor, unprivileged ChRoot, or similar context may be acceptable as well. Routinely modifying, sharing, and running code, may otherwise put both users and organizations at possibly unnecessary risk.
+
+
+# Copyright and related rights only waived via CC0 if all specified conditions are met.
+# *) EITHER, a single file directly output from 'scriptedIllustrator' (which is GPLv3 licensed), OR, not otherwise claimed under other any copyright license.
+# *) Is a documentation script including this message which also predominantly creates or represents markup (eg. 'scriptedIllustrator.sh', 'scriptedIllustrator.html', 'scriptedIllustrator.mediawiki.txt').
+# *) NOT part of a program to compress, embed, and assemble, functions and other code (waiver does NOT apply to 'tinyCompiler_scriptedIllustrator.sh' ).
+
+# To the extent possible, related software (ie. 'tinyCompiler_scriptedIllustrator.sh' from 'scriptedIllustrator') remains otherwise copyrighted (ie. GPLv3 license).
+# Specifically, please do not use 'scriptedIllustrator' code to distribute unpublished proprietary means of creating 'current_internal_CompressedFunctions' .
+# Specifically, please do not misconstrue this copyright waiver to negate any copyright claimed when such a documentation script is part of another project or another copyright notice is present (ie. 'otherwise claimed').
+
+# 'For the avoidance of doubt, any information that you choose to store within your own copy' ... 'remains yours' ... 'using' ... 'to publish content doesn't change whatever rights you may have to that content.'
+# Although this project has no relation to TiddlyWiki, as stated above, vaguely similar copyright principles are expected to apply. - https://tiddlywiki.com/static/License.html
+
+#__README_uk4uPhB663kVcygT0q_README__
+
+
+_document_collect() {
+# NOTICE: COLLECT
+
+# Not necessary. Warnings about 'command not found' to 'stderr' will be ignored by script pipelines.
+#! type -p 'recode' > /dev/null 2>&1 && recode() { false; }
+
+
+currentByte=8
+
+RECODE_markup_html_pre_begin=$(_safeEcho "$markup_html_pre_begin" | recode ascii..html)
+
+
+export current_lorem_ipsum='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+
+# NOTICE: COLLECT
+}
+
+
+
+_document_main() {
+#a
+#b
+# NOTICE: DOCUMENT
+#__HEADER_uk4uPhB663kVcygT0q_HEADER__
+_t 'Copyright (C) 2021 mirage335
+See the end of the file for license conditions.
+See license.txt for flipKey license conditions.
+
+
+A program able to call other programs and extensively self-test. Compatible nominally with at least typical Unix-similar and MSW distributions.'
+_t '
+
+
+'
+_heading1 'Usage'
+_paragraph_begin
+_o _messagePlain_probe './ubiquitous_bash.sh'
+_paragraph_end
+_paragraph_begin
+_t '
+
+
+'
+_page
+_heading1 'Design'
+_paragraph_begin
+_t ' '
+_paragraph_end
+_t '
+
+
+'
+_page
+_heading1 'Safety'
+_paragraph_begin
+_t ' '
+_paragraph_end
+_t '
+
+
+'
+_page
+_heading1 'Reference'
+_paragraph_begin
+_t ' '
+_paragraph_end
+_t '
+
+
+'
+_page
+_heading1 'Copyright'
+_paragraph_begin
+_t '
+This file is part of flipKey.
+
+flipKey is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+flipKey is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with flipKey.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+'
+_paragraph_end
+_t '
+
+
+'
+#__FOOTER_uk4uPhB663kVcygT0q_FOOTER__
+# NOTICE: DOCUMENT
+#y
+#z
+echo -e '\n\n'
+}
+
+
+
+# NOTICE: Overrides - new functions .
+
+
+# NOTICE: Overrides - new functions .
+
+
+#####Functions. Some may be from 'ubiquitous bash' .
+#_compressedFunctions_uk4uPhB663kVcygT0q_compressedFunctions_uk4uPhB663kVcygT0q_compressedFunctions_uk4uPhB663kVcygT0q_compressedFunctions
+current_internal_CompressedFunctions_bytes='11208'
+current_internal_CompressedFunctions_cksum='3845181394'
+current_internal_CompressedFunctions='
+/Td6WFoAAATm1rRGAgAhARwAAAAQz1jM4YtOIF9dAC+ciKYksL89qRi90TdMvSwSEM6J8ipM2rR/Iqc/oYbShD5P+hKgz3ONSu7BhrUf8OSN4oZ8BL1e7m0JQ33pEQs007VTHA7nLczyIuWiilZSo+0zB132
+DrV189uAlZ6oqD3MK7bjrSmuGreEaBOC+z5QkGUPIDVaXfJmDg73/A1Y9JqRKxtli7ZDurfX2t/Z3m6RV6ku3LwPHl2qt8/kbWEubRkY3Fl2VTTFWjQ8Z9qfpBK4YyV7fw3X3hcUmN6Fz+u2P8eCSl/fCnNX
+HtvGeiwoJbZ3wje2iPvqVhHoy0BMVkEDcSqWo+znkV0BwqE45qLsZQ9IGG1CzglgciwcOU2fdsqKBFC5XA3WYFGg6uZ2q4mvn5jWR+FbeUz7YjupLAvLl7DE+daOBWzzyYeFrcMhDk1QeqOKpv41V0GTTqAm
+Z7lRbPrhk3oRY/kBiQGZSfRuxTSmIc1zevrFy6JWcZkCJayU/wQ+XEKdvF51XHH+OYrSuhIxvk6G+Ce0sbTr3GBfVRQYdRpxeOs48xNaEfmBf6GZcWUMx5L3LA4cup19vL4O75JjKbnlJeYdoRCjtcPE7A8S
+82KbKVXWm5n1ADflKsnTX3CVTSu7uS4pVXHlludUnC/0dCPIaO2WxpZTXETnBW0SD0f3HTs7UDEzwk7eMHnoe3xk/cdsh16yyG0jPOBLHEqULsotf/cimpgvkZlFxPNFKvrB2sVBDpjr393u8l5sSqQ9L5Vr
+QgGi6nBnxhDESgC1fMuEusvr6Sli1rR/+c0ap1SksGpO2TEUmhLB2btBz2JAuC6RRnqPSeJB5e7Ye3lLZgz7qHahujxIhELZ6Ig37WMvcLn/BMoXxtrpqAx3WQEFwPU+GEqAYIYrxdNFO0vmXqIu7mijEGNN
+3Kz52PfEP9wuiJegjzcwG1SjieemTZAy5HeBzXmyNh8NuIdpHzxX83tVQ6zn4W8eFh94YUkd8LPxqapxW9+5Ljru6KBXErt6eoQ1JzryMghYRXroIw6cezM5nO29ERbDgwMaWHM8HHSnsRVwOyHrn3h6dSYW
+FRI5yIxuHcZpSxIl+Wcyy7l04D052ZLSwIr3bVXp4Wl8UN43ZjFk500/N03EAhnaDm33ejErcZzg3IyCf9KDULz2vjN1ZMLj0f8szC8Yd5LbbQJ0QaXtts9dioTUAtjmI2n0Nua2iP8U6I2yFW4q1flLmWgw
+dwxuvholhSJhoQ1mvtYG9GHKOnerwBFiSgokDlXpGx/wWLEggdJzD83Gsc970UUKaPclxZq9t53+B7POZLqRi9MT1bN4M5xVNF9BpfIOMoXjjfCBdMsMg9KiWzl5MampEj+sO3zuoQacK51dsC8fF1kx/Ipo
+T0+57cS1KCKaDzB3/U2HEY855HTDewfULt1Mox+DjNi+SoyNEej/2dvZj2k7gc6F2P4cYp0XlGset9wxtH8+3HkTBns+M9aR3+kzI/CW6vSnkHXti9l/usvJEofd55FM+ZveKRq4OmWB9Oq1ruDmZeB3ti5H
+l9Be+OF5pyJ7O4nKeKvq36QuxgvvKFuWDH8YrEDrrLKvQA2ZwqPfB/Dx301jgKu2lHwCC5Cr6asX/Tjo71N7833otVOmbW18/ev2R0aUgZKsqzPZIqFX1x9DpRggWTuQOX49oKJA3lS+k80eCsgjWZ3NwHHB
+JNS03KTfjGNYL21M5FopczHLlOKA3NfxtRVtvC3ptJwtqiQYiRhRFdiUmrckYzPNf/WZmL6FouTxTXHf/8KnqYdZ0a/shLqxbCEkIwYD7IZlBIXfZukhiP6kefFP2XGAvV7HC93XH3BC1oYnPQBRcKtLLn5C
+zda+MyZGKU7AXhq1EleGh62Y/vDec7Bgz3UwJtR+8YBCdQ8/Xcf4mORqcw5ELeSC/sZESq/ZT4uHAWXlbxnvzRXvDJ58sFyghbkx9mNwpWRFrj39Hq3TTm9NkSX/ExPFblReiwO3+1d5czQOEU8JnpKwbXak
+iZTSx0rJ6xz5YqpuDxrnSOA9Ti+KiK6yFVv5gZJsPa9opyNifSPEsLjaULtj3MT0jHSHlwYMPOm3ZU5anWZ9zLnlZDFk+5W9aVR/+HHjmTDgYiD/2lVhvyGFs0RFnIjj0W1Yrr0Qh7RR/STZo3Uj4PsS01jS
+XFU1MAfvrY5cOFVq3gGq/BcYNU22RhC4TjfS9SLCd9Rrp8795lN2bSnGVKQ7eCrYdqYvCzLQ5RjV/jRfze8GD8XuJfOitXktggA4TGfdN2an12568jtS+q125ACbgQsG6KbDJ6HDEIk7PfdymVGeLNP58IfV
+Ti9a8Wv3gxx1o9cP105zLp3eY+BXgXxpJpM0PwQ9C6SDYTYgWhLyJIL3s80HCWKRoNzfKE5+FpmuIodKb6nrgjQM2iMrkzD1grRPrIweqC2IvgUUp76cXjIWapPPJKoRAis9rOzsqZfOqwe4dCaJPjS7hodF
+HOUHrNeKwCtOP73IH4yMLIWcTQkcR4cH2ycD+GoyrsM1KfK8rRf1sA2exEaj1gBbL2X90zTFijyIQp5ACcTa7g2erPu7xNzRNLL4lcAtRuduONBy7H3bokwhxUVT9XYKK6vxOsGBN9rqJ32dBCki2eznhSvG
+YY7hDmldozOVzv70Ht4XupfIlhujDJ4CAVyp20SPKzFsyuI5O47AKB7bJ6KeNvsN3dn8PJFniSfpCxiksP4mSGD6c4fFUOKU1sqta3v8sgkEZIxqjtcEVkfWMvK6fVIb6z66P2kDSqBE++icHbYkIr+dczrx
+6GLgOXvNFml1WJUyhpmyhkIJzZ/geu6ET9wonX4MZ/NEmC8jxUNFR/hfM+yH2sYL+1K4zI+kNhe0+iPdJYSpbxC3hOlYg5wsF0a72YOfikpkwp+2SdL5ItnyCkVTqMJVpDMSDppFsb1IcSm3oVLlTCOYpQxi
+xNDpvrwfzm5INJXutfAgTnsuAAcaEIojAwo9N/+SBWcckzuXDR4OwDi3RV1muOvBjokJxIdAPtWJ+uNEVP0lXh+h0NtQ7ef04SfuYp9LQG1xMkglcvFZ44jp/L5G4NN4WnJJhKab539tSqdbMdDrRiLrsuC4
+Kqfcc3ACQ7smygeKtLcR6Yu2XbWeFQp7xXF64QANaauEn1B468J6DKVmL9sPEn9OKeyzE+rXG5BLgwnmxGp2g3Rhxx3y6gsH/wbcZt/EkkeDHELsx5sk8KyriY2Yb1OjUYeiQWCV745Tq8BbqT0JTEOXJFqY
+ZkfG/WYAkps/wfTT+b+czthQNz8B0JuJntuzO/HsdY7JSr7SPeY7cAMvkDbR1epRioPKBodiUom92THF/8n9TImGVLJ3lckK+aWCvhBJob0hGYjwLjOMstBkRebvv9bo2ANqu8BQN91NEoeR8VkBiUy2ehI3
+Ul+mzc8lMLKxtat5c2eyVLu3rHb+iA3WUBs6CPP+iQ6TVQ7XmqVVcgxi8is8XQmiQGd2yAqYV85msD8IFnuqu7VI7CvrB04tHBYhT6LaUl8Wbsy7p4T2J3LgCkZAKtC0/8dWF9Pw2YBV041WL6VpclpjvyzX
+iUcR1tcsAYFY+JkMV2+m9Z/EfWTUVq0Bw+lU+zuJVIA5wRFyr82TK8co+1athO+Jb6wDwUZlKCbjv6FfDwbM7YSeXMfZyv1xWnD0DRljF+47kBHrLpC9LodM12kpS7DFP2JM+qrKI+dOlYyqss6SBE3ulVGN
+WpAklVS+q6XfmxKybZktfJMBVF0D6foOmV8mzrzo0/p9p0Zr1O+XyR0NRpYlvcDs+lN1R/sxT1PEy8dhpDSQXw+LTZeeNyFxrMOfRlf6YzjraAXQrXn1gdfqVObw/ZhSwwPyDd9LpfxHzKG7QQHE7hfl+Evx
+oQxAxdXG3wXWdRwkKG4nTxji/gDKadNFslALspS7pO7isIsvWI3TxmpJdAHEXaMi/0SLniCnQ3LhAIv5dcutwot5mZoFCuKajSHI+kOAAOhg+LNVw4ffXC1QXEgsDfq8dN2ExSqZEHlRi5LdOin6iFuGLXgu
+G1UlvO4Tk2JwV4jUfj3fL81js6XdUAaZPQqIV2duze1kw57XewLN04NTuuKTMcClXjT+SEpx+k7uYx1IP0aA+o3W6BH7HfMtPeNsLdaiqyP0mwZftiY6HY4deCB85ZPkxAdmYtgyGIJkKB39rbZGoId1d+bi
+jigAiPEYJRZIPGlJFVrshXAW0aznlO0ZVYWC1ayKf/MYkehIGNhx+8xY/iAl+eWsQ1F9B5gzg2nx3KY3oYPpqANc2DPJYjhOqXiu74ZhlyiTcWwNTHafdqQyfYbUgPOtvm+wdOoVVwAG4PnFHkwLlZRJV/3t
+1xS/bmbIG7Xfw8dZefFLaK6D9bvfn+QvSsaOE6C88HMLRiHSOBdCD48v5J8zGAsKxJAmABMXslSZ60QhFeulEirWjGezYHEUM51FOvIb23EP2db6EjzPpaX0M5bklZlMwJJRKEuSBD8kqFQjMy2qNDhO2FvH
+LMpdFKeFTroh9pxRM5u4RPUgorqCkwNXnH4x1P3w/NGCQXiABX3+6mkto3MzIRphgCj30TuxEwm/e87D0VKLemwkHe+OppR947TYtyHogjgtfUBVPJrsv6MScTNYGtPOPd2f5pIsfrUBIPbB3itPaSFQ72mj
+mPneqpbrYHmiiNG2U8b7MG/iNlfP0NbwGPhqWFlDpwZsid2V1nc5+J86Z7Alqq9jrC9F1OzhWnMQlRhoB20WSKzTZtSYYtwJOtnURt1+7BA60T1OgqseRqnKE0O3wXiv3t8auRGFkMYsWob39UhnfBWEtTzM
+941rV5JHmEikvUeupItKxxzvz4MwLI+GOQrQjq62P/L+iS9rQC0lZUsP0ase7yaOAzJwHDJxAzuFt0+N21Q4IkzmbFe8j+Ott6Tjp49ptCaBCXrYg4UuDKnTPS6d733fVjm6uE5bLDnGMONXfdKuONqUX/ki
+ATtlPwwQFAQiaaS3Rp8T0s4JW/gP+mkFNeqfg7ZWTtNSY3OoUEDlXdvYTC2H19dwRMEQPKexzpDJP2y9j5yAag0sWo9N7JpmlhLhYuFxHDhxV9B9WhvtEPT0MGbo3S6G+txwbu4VAH3J/3/hqdpz5JFx3YUf
+/v+qxZWKW47yVIAm3ZhJvtuDoYMOQumfoFTiVmIRrNonan0GlPeASyscmdc9mwGGnMBhW9kJLzSdcSaVwbGvuMMRWrKWFZHpfqmzYBVo523Sj9QeXDuey77LW97bcAWlT9VyShspWQwJUN9oAUf5MgCfmI4T
+D7h5ckGgQimXEjx5qjG07rs+9f+2AVGAKYlH/cPswQ1F62Rty9KyFMzgGRg71B4I8vr8LlxD/3U4jTGfCTSbuyblJOJRTzJOgn9kB2mDth3TF7qc1XzNBP3NP11R2Mo/ZnXBg/eK/VkKDwRGFtXQkX61cGuj
+xMcXJCN63XMrA8V6bqgEswJTBrJSG6Env6dFdRTxlWxpkpwNX2/LXSTvc65lHuX0/6D6XCLHw9uLzeXYlOaYTsUtRQwhizQ4QdO4f+EWpAhRPBKnSIKUzYmTgu5Pz7ZigHZHWAFXN1x6iGN2utO34rgZZChQ
+O3a+BGb7epgLN9bYpy40PZj+eB+aUAlgXMZtJiygRfwrLjR3O+Byg7UKbGTgfCikBb6OyKkzukgMxOZGvCyRjiqK7ZHc1psmXLYdcVNvIt7VoIH95e8bKYg4ppG0GATf3+63N3abKhUryQif2w2AX+Wy26uK
++cCFv5x7oxpZ2u0KIjNEY1BOpkn7KuGX6nQNGU8kcoPhJCrb8VyZv0tJRKmDv+jLNEDPygmIbJvO02Qdkny8GrJ79wn74cUkS9NuVbRc5BscOcyWb/xfVTkGOiB+jCb5AYzLW3oogXiynDOdZZMvxZd0hig8
+yA2ZIBI9fCCxLAQswg1e7kk9GxK3SV0Y5OlOsG1IkW48evyfbaOlHmW+yfJw6cnG1dMRr6t3dwTDdxQfdnNuR5UD1fgKnZ7xLtCSdIHhLw8qoWD7ZGkp7rngHIGt9gn1sKvlyG5ul0b31rflQy8WI6RVfksj
+Hz/lLW3SWafGEYaNVEOZ7UEwZo8wsmxvyWki50xQTOAzY6qxD2nKD40zVXwHcciCaAvkbYeVcjNUzxbdNa140ND6FRTyp9nF0s+Kp+E+3CLSfp0IeHOPPgJO1+F3h0E8KO+xK2hbAm+2Q/tq/KzDC7MqSk/M
+ZULneMdkycKo9q1YJhHPoXQtugKtxxSoW8jPkswZ7tTfuiQlWm8extuBnqAJKbCIPALlvaQOIOJMzKmjxGkyASKpTjErQJS3zDlMtckKuMo66ZnvzeTdD8ocSW8ygVYH53HBgUScoGXyXRp/MGtrUSRjMMtf
+Ktl3Wl2stKgwmxR5ct2LAQtgbJe5RFOV+Nj8agKNqZYxq5nunYcQHjN+ZdQ3mJvXPr84/r6VqzFNky7Pn+VK9CsiY2eRUsFq70cpIeX8P4w+emETGEIPrmtFou9Gd2s6aYBFvPWzzc2lz2aihE3N8cSOXOjP
+yOwavdJkaiwkONtiOHBUG6ENvv7Ygc9Qg6ODCAd8lfUGglFvOspJyZ0rydR2cVqQ9DI6cGOJh4QKcKRBvBTRNE52gLuBqZP0j3V4t+S8qooqXHWQSeVOR2Mc9kXbmIQna2K0C+8amarnuAaLji9zvx300bfj
+b9qpcmASCooMbFE9d8nXf3qRcI10avbAalEw0WbUyXslNvDNqjon1evcv5q92vbw4lU5fCc1jsPUXDM3FCCOxUU3bu05cwj8COUV5ZtPUiNvUFTaFLaIdJoqvbgWchNhosD/87J7P9p6FrfZAIvW7qKoM8bL
+IjAPWEAQ8+bmaGYL02N3jQaGLxiuFkSrWLq65bm7yXftNJ7aixzMBbAFgbKIflzXK88bYP0vs/y15NyRgbMagT30XXzbxO/oOBYxUT59bqCOC5GTKNQ8sdPmBQhVJDM6fAHXphBIfIYcSVXizlYFxuwLVgWW
+Ly7kFQ5p4DVjN6aGnznB4Gdco+mMebrURNsc71JLtffcSbQqzOyS4AlDLHwOJkAqivBlluiAcpDmeb4DYZffLn9iDxLeDH1aCbUiY3mY1cfCxMnaPQhfaT5EG3SQ0Io+x1jIJXYqxw//RRs128TowQ45tk4r
+LnsoJJONNNMp/tP9B8hQ1bBT13Q5Dfryb5uZgxr6u7LGMTzKSj37muJn78rXueVUWPC+QXLsmgjdkeScO8sGxG3CbhuEDJCiXt9guDWHWIjFpm4dXs0wW6z/bz02tdIm/cYtO7QNneD8i5U9+1dwZFwm6Ha1
+ezW9ucXXuoElsXxHCd2bzVdqD9LDHAEprBU5mefH1O9buIfjCxQqY1r3sXAgND5PwPbecDTlLgxmdECn2Apey/ucQIyAVBOj3RVQsWFh6dsqpPZsHNb3HUzcnfxrqZDqsf9TF0tRSzvggxEjWVzrdcwn6Mwx
+kWZVP4LvmJTxKs+R71HhmXQ4C1XpLsLq5xxK1AfCZJXb64D2dSXWlMGOEFhSNNaDM7XyLBXJ2ilYiKvC1ZqVPj3JLJMttHXcWV7At8op+nC8qYszc706RiC2mPPb+B14KW7d2NeIL+aFP9wraWhvxulwpjo1
+M82rwiyYhmmcM2+9UANUsS5QZiSW1yFHXLxPXIYTodIuw3+v0EdVxqlZ2Z+WqnnRXzJQkBvb3A1TeBQCUhJusAs1gbl4VZaMqtLZoe5oKeFoJuad6BpFEd05b2rVJ2G7ME1eA+EhX/egz3wvLGLCpv26Kz31
+XilcAhApN9hrxSLp8cMHRzJ4nd739Z5Qzi1WVYw6w7jy+siXsCJmD8ZFbCQMIbyDTwKXY/CEvy7A3GBGVS7P0UsnxalScd6M/89kibMDOr71kf4xw/c7LghuogTucaMQZHElcKcp5j6HqIBrC02nhdHH6Aov
+V20R+gHD/tLpfiICP0HWO4LF5CC7ARHFT4/L3IUsPODzEczb5sKxf87FpRblBUUshJ+cZDXmBcH1gDY+vLCub1YGpcf3jb8QL5yIPutfVoSJtC/34cpOp8kCTzcJFtDPD9z02UES9k0wS3zak8P8SaxyAtgC
+cXviDrnP7YtD7cex8ug2w2hpxr+mPLwm46biTlDICaXs5J3q40cvGFK9uIiQXf9ROdfKziw9F00By82ISxvSNxEk16YxjoRYenz7P3jlP/lzKr15hl4Uvbd8ghQay8HwqPZx0z3ePFgYUqTdVAkIkQkmnD3l
+KvVzQK5j4W4+35yJmVzcSAsb+ptiatHQL6dRVKOQ7AdzjKqVPebvzIfVq8wnodVGG7NTG1V1SULkCi8OrqLNX5A93S8wrognvoWMuLhCQs/Fs3nV/BU8PA7ngqyei1J6Po4c75qWyEnRdDKGEQpmpH3kEJSC
+FoFmfFoFopfX9af1VnapkcTiiNrc1dl/loJbmomLM0Titm5Uz936gOEHaXRAFfZ0rogKSMbR/eHRCIWbP1MEogzY32uqOZeQ0IYVpVJRpV9Sx77CVvNte7tQF6KADkhrJf4M0KDQtcfqKKr2ijuh72zonhI+
+QcZTWKZDjmqTbT1oOLITyQatNs2D7XWORZxPlCmnN0xdW1UdvME1JqakGO7p/cRgR9rvxafQLwMhxXhY0icxJF7VwJbfJsJ32zY3sZFyOJwmBdYJdBVk11iXekzlUOUCLCrRNp5xvc38Di6iaLd7fXFn8uLe
+N8PwIahN4IZvDf+vpdVYHKUhYfvuOE6XD7tIA5d3Omb+QiOm0Dy4u3LJFx2t6ofoN2ixC96PKy1eSaYQdnDNNBK/Q0yMnSnD8RZXCQrMnimEK/uSL5znzZ00T65bPOHTglSgdZY38epuVc4bavlNWwpQgsaH
+vhVpEM67ar/5irP6kH6DPhJt1Ib5xwkadPbhjiHg1NhS8X7doWph2d/HAACikSFsntzCfJlzYh9er9YrQxrPYnE95DQ387AFuFI2HagszahUoQmAx+wrJHDCPV0bWhisB/gYerO4rvewQe5syKbJAV/A5RbS
+rYm0FXaIrW7s38bPACBZjZ8Kn2m01XWnUrrcUMhAzZ5V52KLJbAye3ZPZ5gNOIYgDjcawf87BFJRTmVa2uqmgNwp8zS7JvnfgmRx6u9qbByckJcrwL/zquOZd5ePk/UJZ6lF6j5PknIAxRmwreIisC4Jgw7r
+SBUa/GXdYHji7CCfLyQINKlEVm80SHxt2jn/tx/15wt5anvWuZCkCxfNhCUwyEhvShE4quAcmXl7usc2Gvyqa64fCle/Qxg2hNJDh6jtXYiWNjr/Hk7X6vt4MkcfKMpvy0ZHWZTVPGwc9rmspDCgb64/shoJ
+YTFvHx3W9v1TBpahqUBeWo26LoFgtuZVEXdgz2ICqjlQCBy7YswdPWxBzV0+7wc+MFR2mPWpz7Hx2rDmiRee8hHhHLpgLMh2CM4up9SxZ9GGQkKGgoWbSZb30MVUH0F7wNlFQOWQz/C/LdyKa1uCXgJdxaTK
+hndTH5HHjitzTQnqge6X5WtHT/o9+M6aaC0BfSETm2J6ddjuu/LSezbpfAgW+W/UAEY0cRkoAvXEcR619TzR13Lgi2V6PAwVjBDekeBswFjD6+NrTKdDWieUUBMlez8ucvWS8JnQE8kG8TFM5IV9zMjlet/i
+iz+w98wC/L1RgLuBXqF/4rPdJkv/elsEZ+6SsceDZ+vSqwchTRXwnbXpTBJcvJkPL3EiHWMFb2uGC/2ySD7Pi8iSiN3lPa3cLL2WmKo/c7VRRU05Hp4/16G/DNHCNFVYvt3CwwT9ef2xACnsTOiY63m5mtyb
+43cL4NtwrPu9X/76e8CWAzQa3i/T5HJmsAL4Y6To53q3TRew+5xB6i6MbzKL7dBMxtPeXLR0Gl+Hoa2pNa+3VK3kkjVFBdW3oifBa4ZpkQ6f618xsDYUox3SyBOgyyx4rmhHNglytBUDd5F9Udhx8v3BC0ZC
+zYknO6+GmdECq/ldL2j5Ah86o8XK4focRn1pO0IIs6NSCNvFx5EiQPRCuFf7fOFo2nsmdvQQCku8x7wNEu2weuV+sOV9WlhcJzTNhCawSTpn1PcSNWESlnA0Zauh06uq47s+NcNK54fj/f5dS/92q/2e0Zgz
+igT/79Wf3KIBpmjt615cpjB8w/v1vsjMCOxYm0/4QUpMv7jI//xIXzXn+1XLCDuDvqypc0U05xf4JRwX8DMJhFnZjm7qlnBaxLq6lB3ypG3LccYpxzqZIkoZaJDK4OzfBpypcQ2ZE0an+dCJFEvIjPXf9AKM
+mVXxyVTbFk7FgTWXgG4bBXkWmZ1CIMnezJlLQBLgtdSMQs1c+VTUTfPL/OcRjoy1MMvkiiaDFMzGCdVckBbvbm5SZsQHr8jCmb8JZxKVY8K4Gs6Bf7m7PHACNM/GiMT431k2+k5BHPbHSJlfgU+56ouqimfT
+r5UIiU9dMyC6RxG/CIDW35hV7dMUleLfQ7km8WRyo9fVPpneANkzssjf3XQoJy+xJJMv5SVNgi7anUATVJd1856Xaj0+qCpGFSbVfZfPGU+j2tZ1YLY3XfBkK1uFpEhAlkb61rQDeSvvylmXyZsU6a2pCpl2
+aIdkpJcLn4u+ELJrBOfO9mr9PkSCgZkFr7fZ6+ot7A2hCI4VIxmHtUgyRLBwC3+Z7merkBQXqRnnPKOK7pCD6VzkWXxJpBRzz9jGwMip44z3rWD+KSU/ckrt0ogscZjQlymxaIrslporEsRIeLNMOGAACOJ9
+yAH0mgIY8KgCHfEsTeJlcvSPigdV17X05me45oVLtuaFdHkcbdoPy2ELVsNBlD8CdMg+1pEU+bb9cUvscp4j33v3T/tO3TYn0pmT5UpPR2e0nP7kYPfK15Y6ceZAERXlfveldl6YWd7ISB8gg7JqOQUopm+b
+oOJmUR8ZHA9pUhBwqLcj0kMxhHJn220VPNug3WhrPgPq2f3vs0eJ3GDa2lzC1i3uVPWI1Ttxoex2Lfr3aHAdKw/gOaDBbwGMYEh9HQ1LL1CFNz3Z9dLP0mXescFDa1gUGUMgdRAOWEgJkZjMilSxuCUsKPVL
+VA7oAPQbZNHUdx0AAMJHiRseNv0RAAH7QM+WBgDa+BllscRn+wIAAAAABFla'
+! echo "$current_internal_CompressedFunctions" | base64 -d | xz -d > /dev/null && exit 1
+source <( echo "$current_internal_CompressedFunctions" | base64 -d | xz -d )
+unset current_internal_CompressedFunctions ; unset current_internal_CompressedFunctions_cksum ; unset current_internal_CompressedFunctions_bytes
+# https://github.com/mirage335/scriptedIllustrator
+#_compressedFunctions_uk4uPhB663kVcygT0q_compressedFunctions_uk4uPhB663kVcygT0q_compressedFunctions_uk4uPhB663kVcygT0q_compressedFunctions
+! _tiny_criticalDep && exit 1
+
+# Special Global Variables
+_tiny_set_strings
+
+
+#####Import ( 'ubiquitous bash' ) .
+# WARNING: Do NOT invoke complicated 'ubiquitous bash' functions directly (ie. call "ubiquitous_bash.sh" as a binary from PATH instead) .
+# WARNING: If '--call' parameter is changed, 'trap' conflict may occur in some functions (ie. ( '_test_default' ) .
+# Keeps "$scriptAbsoluteLocation" pointing to this script file (not 'ubiquitous_bash.sh' ), intentionally.
+# Import of 'ubiquitous_bash.sh' intended ONLY to provide most recent 'message' and similar functions.
+#_messagePlain_probe() { return; }
+! type -p "ubiquitous_bash.sh" > /dev/null 2>&1 && exit 1
+[[ "$ubiquitousBashID" != "uk4uPhB663kVcygT0q" ]] && exit 1
+current_script_path=$(type -p "ubiquitous_bash.sh")
+[[ ! -e "$current_script_path" ]] && exit 1
+! ls -l "$current_script_path" 2>/dev/null | grep 'ubiquitous_bash.sh$' > /dev/null 2>&1 && exit 1
+export importScriptLocation=$(_getScriptAbsoluteLocation)
+export importScriptFolder=$(_getScriptAbsoluteFolder)
+. "$current_script_path" --call
+unset current_script_path
+#_messagePlain_probe "$scriptAbsoluteLocation"
+#exit 0
+
+
+
+#a
+#b
+#c
+#__HEADER-scriptCode_uk4uPhB663kVcygT0q_HEADER-scriptCode__
+#1
+#2
+#3
+
+
+
+#8
+#9
+#0
+#__FOOTER-scriptCode_uk4uPhB663kVcygT0q_FOOTER-scriptCode__
+#x
+#y
+#z
+
+# NOTICE: Overrides ( 'ops.sh' equivalent ).
+
+_default() {
+	local current_deleteScriptLocal
+	current_deleteScriptLocal="false"
+	[[ ! -e "$scriptLocal" ]] && current_deleteScriptLocal="true"
+	
+	#"$scriptAbsoluteLocation" DOCUMENT > "$scriptAbsoluteLocation".out.txt
+	
+	_scribble_markdown "$@"
+	_scribble_html "$@"
+	_scribble_pdf "$@"
+	
+	local currentScriptBasename
+	currentScriptBasename=$(basename "$scriptAbsoluteLocation" | sed 's/\.[^.]*$//')
+	"$scriptAbsoluteFolder"/"$currentScriptBasename".html _test
+	
+	[[ "$current_deleteScriptLocal" == "true" ]] && rmdir "$scriptLocal"
+}
+
+# NOTICE: Overrides ( 'ops.sh' equivalent ).
+
+
+_test() {
+	"$scriptAbsoluteLocation" _test_default "$@"
+}
+
+if ! [[ "$1" == '_'* ]] && [[ "$1" == 'DOCUMENT' ]]
+then
+	_document_collect
+	_document_main
+fi
+
+! [[ "$1" == '_'* ]] && [[ "$1" == 'DOCUMENT' ]] && exit 0
+if [[ "$1" == '_'* ]]
+then
+	"$@"
+	exit "$?"
+fi
+
+
+
+_default "$@"
+
+
+
+
+
+
+exit 0
+# Append base64 encoded attachment file here.
+__ATTACHMENT_uk4uPhB663kVcygT0q_ATTACHMENT__
+
+
